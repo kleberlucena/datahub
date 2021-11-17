c05e1c8830133742befee23f62c36ca70adc6848
Diogenes <d1063735.ufcg@gmail.com>
2021-11-17 19:46:32 +0000

    Test api inicial

--- project/urls.py
+++ project/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     path('', include('base.urls'), name='base'),
-    # path('', include('auth_oidc.urls'), name='auth_oidc'),
+    path('', include('auth_oidc.urls'), name='auth_oidc'),
     path('api-auth/', include('rest_framework.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
