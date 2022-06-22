15f1b9d60f3ddebbee2578618cc837a362765468
Diogenes <d1063735.ufcg@gmail.com>
2022-06-22 15:51:55 -0300

    Inicio da API de pessoas

--- apps/person/admin.py
+++ apps/person/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 
-from .models import Person
+from apps.person.models import Person, PersonDocument, PersonNickname
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'created_at', 'updated_at')
+    search_fields = ('uuid',)
+    
+
+@admin.register(PersonDocument)
+class PersonDocumentAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'person', 'created_at', 'updated_at')
+    search_fields = ('uuid', 'person')
+    
+
+@admin.register(PersonNickname)
+class PersonNicknameAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'nickname', 'person', 'created_at', 'updated_at')
     search_fields = ('uuid',)
--- apps/person/api/serializers.py
+++ apps/person/api/serializers.py
@@ -4,8 +4,62 @@ from rest_framework import serializers
 from apps.person.models import *
 
 
+class PersonNicknameSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = PersonNickname
+        fields = ('uuid', 'nickname')
+        
+
+class PersonDocumentSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = PersonDocument
+        fields = ('uuid',)
+
+
 class PersonSerializer(serializers.ModelSerializer):
+    nicknames = PersonNicknameSerializer(many=True, source='person_nicknames')
+    documents = PersonDocumentSerializer(many=True, source='person_documents')
     
     class Meta:
         model = Person
-        fields = ['uuid', 'created_at', 'updated_at']
+        fields = ('uuid', 'nicknames', 'documents')
+        
+        
+        
+        
+'''
+[
+    {
+        "id": "UUID",
+        "person_face": [
+            {
+                "id": "UUID",
+                "person_id": "UUID",
+                "image": {
+                    "path": "https://...",
+                    "path_thumbnail": "https://..."
+                }
+            }
+        ],
+        "person_nickname": [
+            {
+                "id": "UUID",
+                "person_id": "UUID",
+                "nickname": "alcunha"
+            }
+        ],
+        "person_document": [
+            {
+                "id": "UUID",
+                "person_id": "UUID",
+                "document": {
+                    "id": "UUID",
+                    "number": "123123"
+                }
+            }
+        ]
+    }
+]
+'''
