79caab629d937e6bf1859740c2b0b85f54affb60
diogenes <d1063735.ufcg@gmail.com>
2022-06-29 13:44:42 -0300

    Serializers de Person

--- apps/person/api/serializers.py
+++ apps/person/api/serializers.py
@@ -46,51 +46,15 @@ class PersonImageFaceSerializer(serializers.ModelSerializer):
 
 
 class PersonListSerializer(serializers.ModelSerializer):
-    nickname_list = PersonNicknameSerializer(many=True, source='person_nickname')
-    document_list = PersonDocumentSerializer(many=True, source='person_document')
-    address_list = PersonAddressSerializer(many=True, source='person_address')
-    image_list = PersonImageSerializer(many=True, source='person_image')
-    face_list = PersonImageFaceSerializer(many=True, source='person_image_face')
+    nicknames = PersonNicknameSerializer(many=True, source='person_nickname')
+    documents = PersonDocumentSerializer(many=True, source='person_document')
+    addresses = PersonAddressSerializer(many=True, source='person_address')
+    images = PersonImageSerializer(many=True, source='person_image')
+    faces = PersonImageFaceSerializer(many=True, source='person_image_face')
     
     class Meta:
         model = Person
         fields = (
-            'uuid', 'nickname_list', 'document_list', 'address_list','image_list', 'face_list')
+            'uuid', 'nicknames', 'documents', 'addresses','images', 'faces')
         
-        
-        
-        
-'''
-[
-    {
-        "id": "UUID",
-        "person_face": [
-            {
-                "id": "UUID",
-                "person_id": "UUID",
-                "image": {
-                    "path": "https://...",
-                    "path_thumbnail": "https://..."
-                }
-            }
-        ],
-        "person_nickname": [
-            {
-                "id": "UUID",
-                "person_id": "UUID",
-                "nickname": "alcunha"
-            }
-        ],
-        "person_document": [
-            {
-                "id": "UUID",
-                "person_id": "UUID",
-                "document": {
-                    "id": "UUID",
-                    "number": "123123"
-                }
-            }
-        ]
-    }
-]
-'''
+    
