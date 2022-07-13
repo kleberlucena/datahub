ff0c09722300f99d4b3aaa8926d89e85627f896d
diogenes <d1063735.ufcg@gmail.com>
2022-07-13 00:41:16 -0300

    Code happy

--- oauth2/views.py
+++ oauth2/views.py
@@ -7,9 +7,6 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-
-from requests.exceptions import HTTPError
-
 from social_django.utils import psa
 
 
