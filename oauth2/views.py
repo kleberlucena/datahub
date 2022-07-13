a6595c8ffbae7873b609932a7b7c4c9c1487fb9b
diogenes <d1063735.ufcg@gmail.com>
2022-07-13 00:40:54 -0300

    Code happy

--- oauth2/views.py
+++ oauth2/views.py
@@ -7,13 +7,11 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-import logging
 
 from requests.exceptions import HTTPError
 
 from social_django.utils import psa
 
-logger = logging.getLogger(__name__)
 
 
 class SocialSerializer(serializers.Serializer):
@@ -30,49 +28,25 @@ class SocialSerializer(serializers.Serializer):
 @permission_classes([AllowAny])
 @psa()
 def exchange_token(request, backend):
-    """
-    Exchange an OAuth2 access token for one for this site.
-    This simply defers the entire OAuth2 process to the front end.
-    The front end becomes responsible for handling the entirety of the
-    OAuth2 process; we just step in at the end and use the access token
-    to populate some user identity.
-    The URL at which this view lives must include a backend field, like:
-        url(API_ROOT + r'social/(?P<backend>[^/]+)/$', exchange_token),
-    Using that example, you could call this endpoint using i.e.
-        POST API_ROOT + 'social/facebook/'
-        POST API_ROOT + 'social/google-oauth2/'
-    Note that those endpoint examples are verbatim according to the
-    PSA backends which we configured in settings.py. If you wish to enable
-    other social authentication backends, they'll get their own endpoints
-    automatically according to PSA.
-    ## Request format
-    Requests must include the following field
-    - `access_token`: The OAuth2 access token provided by the provider
-    """
-    logger.error("Ai dento 0")
+
     serializer = SocialSerializer(data=request.data)
-    logger.error("Ai dento 1")
     if serializer.is_valid(raise_exception=True):
         # set up non-field errors key
         # http://www.django-rest-framework.org/api-guide/exceptions/#exception-handling-in-rest-framework-views
-        logger.error("Ai dento 2")
         try:
             nfe = settings.NON_FIELD_ERRORS_KEY
         except AttributeError:
             nfe = 'non_field_errors'
-            logger.error("Ai dento 3")
         try:
             # this line, plus the psa decorator above, are all that's necessary to
             # get and populate a user object for any properly enabled/configured backend
             # which python-social-auth can handle.
-            logger.error("Ai dento 4")
             user = request.backend.do_auth(serializer.validated_data['access_token'])
         # except HTTPError as e:
         except Exception as e:
             # An HTTPError bubbled up from the request to the social auth provider.
             # This happens, at least in Google's case, every time you send a malformed
             # or incorrect access key.
-            logger.error("Ai dento 5")
             return Response(
                 {'errors': {
                     'token': 'Invalid token',
@@ -82,24 +56,19 @@ def exchange_token(request, backend):
             )
 
         if user:
-            logger.error("Ai dento 6")
             if user.is_active:
-                logger.error("Ai dento 7")
                 token, _ = Token.objects.get_or_create(user=user)
-                logger.error("Ai dento 8")
                 return Response({'token': token.key})
             else:
                 # user is not active; at some point they deleted their account,
                 # or were banned by a superuser. They can't just log in with their
                 # normal credentials anymore, so they can't log in with social
                 # credentials either.
-                logger.error("Ai dento 9")
                 return Response(
                     {'errors': {nfe: 'This user account is inactive'}},
                     status=status.HTTP_400_BAD_REQUEST,
                 )
         else:
-            logger.error("Ai dento 10")
             # Unfortunately, PSA swallows any information the backend provider
             # generated as to why specifically the authentication failed;
             # this makes it tough to debug except by examining the server logs.
