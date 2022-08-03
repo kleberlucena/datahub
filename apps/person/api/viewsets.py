44aa15a26f24bb5f9f34a303204a028c9f48c0c4
Jovennan Ramalho <jovennan@gmail.com>
2022-08-03 20:28:40 -0300

    Adicionados endpoints para POST de Face, Tatoo, Nickname, Physycal, Document e Address passando o uuid de Person

--- apps/person/api/viewsets.py
+++ apps/person/api/viewsets.py
@@ -1,5 +1,7 @@
 from rest_framework import generics, viewsets, permissions
+from rest_framework.mixins import CreateModelMixin
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from apps.person.api.serializers import *
 from apps.person.models import *
@@ -23,4 +25,77 @@ class PersonRetrieve(generics.RetrieveAPIView):
         instance = Person.objects.get(uuid=kwargs['uuid'])
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
-    
+
+
+class PersonAddFaceView(CreateModelMixin, generics.GenericAPIView):
+    queryset = Face.objects.all()
+    serializer_class = FaceSerializer
+
+    def perform_create(self, serializer):
+        person = get_object_or_404(Person, uuid=self.request.kwargs['uuid'])
+        return serializer.save(person=person)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class PersonAddTatooView(CreateModelMixin, generics.GenericAPIView):
+    queryset = Tatoo.objects.all()
+    serializer_class = TatooSerializer
+
+    def perform_create(self, serializer):
+        person = get_object_or_404(Person, uuid=self.request.kwargs['uuid'])
+        return serializer.save(person=person)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class PersonAddNicknameView(CreateModelMixin, generics.GenericAPIView):
+    queryset = Nickname.objects.all()
+    serializer_class = NicknameSerializer
+
+    def perform_create(self, serializer):
+        person = get_object_or_404(Person, uuid=self.request.kwargs['uuid'])
+        return serializer.save(person=person)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class PersonAddPhysicalView(CreateModelMixin, generics.GenericAPIView):
+    queryset = Physical.objects.all()
+    serializer_class = PhysicalSerializer
+
+    def perform_create(self, serializer):
+        person = get_object_or_404(Person, uuid=self.request.kwargs['uuid'])
+        return serializer.save(person=person)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class PersonAddDocumentView(CreateModelMixin, generics.GenericAPIView):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+
+    def perform_create(self, serializer):
+        person = get_object_or_404(Person, uuid=self.request.kwargs['uuid'])
+        return serializer.save(person=person)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class PersonAddAddressView(CreateModelMixin, generics.GenericAPIView):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+
+    def perform_create(self, serializer):
+        person = get_object_or_404(Person, uuid=self.request.kwargs['uuid'])
+        return serializer.save(person=person)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
--- apps/person/urls.py
+++ apps/person/urls.py
@@ -13,5 +13,11 @@ router.register(r'', viewsets.PersonViewSet)
 
 urlpatterns = [
     path('api/v1/<uuid:uuid>/', login_not_required(viewsets.PersonRetrieve.as_view()), name='retrieve_json'),
+    path('api/v1/<uuid:uuid>/face/', login_not_required(viewsets.PersonAddFaceView.as_view()), name='add_face_json'),
+    path('api/v1/<uuid:uuid>/tatoo/', login_not_required(viewsets.PersonAddTatooView.as_view()), name='add_tatoo_json'),
+    path('api/v1/<uuid:uuid>/nickname/', login_not_required(viewsets.PersonAddNicknameView.as_view()), name='add_nickname_json'),
+    path('api/v1/<uuid:uuid>/physical/', login_not_required(viewsets.PersonAddPhysicalView.as_view()), name='add_physical_json'),
+    path('api/v1/<uuid:uuid>/document/', login_not_required(viewsets.PersonAddDocumentView.as_view()), name='add_document_json'),
+    path('api/v1/<uuid:uuid>/address/', login_not_required(viewsets.PersonAddAddressView.as_view()), name='add_address_json'),
     path('api/v1/', include(router.urls)),
 ]
