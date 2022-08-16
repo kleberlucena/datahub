e3eaa9943831214796834045761269b73ed5c60e
Jovennan Ramalho <jovennan@gmail.com>
2022-08-15 22:31:59 -0300

    configurado na interface de admin e Tatoo, Physical e Nickname

--- apps/person/admin.py
+++ apps/person/admin.py
@@ -31,6 +31,48 @@ class FaceAdmin(SafeDeleteAdmin, GuardedModelAdmin):
 
 PersonAdmin.highlight_deleted_field.short_description = PersonAdmin.field_to_highlight
 
+
+@admin.register(Tatoo)
+class TatooAdmin(SafeDeleteAdmin, GuardedModelAdmin):
+    list_display = (highlight_deleted, "highlight_deleted_field", "uuid", "label", "foto_preview", "created_at",
+                    "deleted_by") + SafeDeleteAdmin.list_display
+    readonly_fields = ['foto_preview']
+    list_filter = ("created_by", SafeDeleteAdminFilter,) + SafeDeleteAdmin.list_filter
+    exclude = ()
+    field_to_highlight = "id"
+
+    def foto_preview(self, obj):
+        return format_html(
+            f"<img src='{obj}' width='120' height='120' style='border-radius: 50% 50%;'/>")
+
+
+TatooAdmin.highlight_deleted_field.short_description = TatooAdmin.field_to_highlight
+
+
+@admin.register(Nickname)
+class NicknameAdmin(SafeDeleteAdmin, GuardedModelAdmin):
+    list_display = (highlight_deleted, "highlight_deleted_field", "uuid", "nickname", "created_at",
+                    "deleted_by") + SafeDeleteAdmin.list_display
+    list_filter = ("created_by", SafeDeleteAdminFilter,) + SafeDeleteAdmin.list_filter
+    exclude = ()
+    field_to_highlight = "id"
+
+
+NicknameAdmin.highlight_deleted_field.short_description = NicknameAdmin.field_to_highlight
+
+
+@admin.register(Physical)
+class PhysicalAdmin(SafeDeleteAdmin, GuardedModelAdmin):
+    list_display = (highlight_deleted, "highlight_deleted_field", "uuid", "label", "value", "created_at",
+                    "deleted_by") + SafeDeleteAdmin.list_display
+    list_filter = ("created_by", SafeDeleteAdminFilter,) + SafeDeleteAdmin.list_filter
+    exclude = ()
+    field_to_highlight = "id"
+
+
+PhysicalAdmin.highlight_deleted_field.short_description = PhysicalAdmin.field_to_highlight
+
+
 # @admin.register(PersonDocument)
 # class PersonDocumentAdmin(admin.ModelAdmin):
 #     list_display = ('uuid', 'person', 'created', 'updated')
