ce2cdb885fdad2f7524b03485e4ca04f1524379f
Jovennan Ramalho <jovennan@gmail.com>
2022-08-25 20:25:09 -0300

    Retornando as permissões de editar e remover junto aos objetos

--- apps/address/api/serializers.py
+++ apps/address/api/serializers.py
@@ -1,12 +1,22 @@
 from rest_framework import serializers
 from rest_framework_gis import serializers as geo_serializers
+from guardian.shortcuts import get_perms
+
 from apps.address.models import Address
 
 
 class AddressSerializer(geo_serializers.GeoFeatureModelSerializer):
+    permissions = serializers.SerializerMethodField('_get_permissions')
+
+    def _get_permissions(self, object):
+        request = self.context.get('request', None)
+        if request:
+            perms = get_perms(request.user, object)
+            return perms
+
     class Meta:
         model = Address
-        fields = ("uuid", "street", "number", "complement", "neighborhood", "city", "state", "region", "country", "zipcode")
+        fields = ("uuid", "street", "number", "complement", "neighborhood", "city", "state", "region", "country", "zipcode", "permissions")
         geo_field = "place"
         
 
--- apps/image/api/serializers.py
+++ apps/image/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from drf_extra_fields.fields import Base64ImageField
+from guardian.shortcuts import get_perms
 
 from apps.image.models import *
 
@@ -7,7 +8,14 @@ from apps.image.models import *
 class ImageSerializer(serializers.ModelSerializer):
     uuid = serializers.UUIDField()
     file = Base64ImageField()
+    permissions = serializers.SerializerMethodField('_get_permissions')
+
+    def _get_permissions(self, object):
+        request = self.context.get('request', None)
+        if request:
+            perms = get_perms(request.user, object)
+            return perms
     
     class Meta:
         model = Image
-        fields = ['uuid', 'file']
+        fields = ['uuid', 'file', 'permissions']
