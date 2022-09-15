c6ddf3bd5f4bf59425bb3a0b27d63bfafb2d36ce
Jovennan Ramalho <jovennan@gmail.com>
2022-09-15 12:32:54 -0300

    Endpoints para migração dos dados legados de Pessoa

--- apps/person/api/legacy/urls.py
+++ apps/person/api/legacy/urls.py
@@ -7,5 +7,11 @@ app_name = 'apps.person.legacy'
 
 urlpatterns = [
     path('<str:username>/', viewsets.AddPersonView.as_view(), name='add_legacy_person_json'),
-    path('<uuid:uuid>/face/<str:username>/', viewsets.PersonAddFaceView.as_view(), name='add_legacy_face_json')
+    path('<uuid:uuid>/face/<str:username>/', viewsets.PersonAddFaceView.as_view(), name='add_legacy_face_json'),
+    path('<uuid:uuid>/address/<str:username>/', viewsets.PersonAddAddressView.as_view(), name='add_legacy_address_json'),
+    path('<uuid:uuid>/image/<str:username>/', viewsets.PersonAddImageView.as_view(), name='add_legacy_image_json'),
+    path('<uuid:uuid>/document/<str:username>/', viewsets.PersonAddDocumentView.as_view(), name='add_legacy_document_json'),
+    path('<uuid:uuid>/tattoo/<str:username>/', viewsets.PersonAddTattooView.as_view(), name='add_legacy_tattoo_json'),
+    path('<uuid:uuid>/nickname/<str:username>/', viewsets.PersonAddNicknameView.as_view(), name='add_legacy_nickname_json'),
+    path('<uuid:uuid>/physical/<str:username>/', viewsets.PersonAddPhysicalView.as_view(), name='add_legacy_physical_json')
 ]
--- apps/person/api/legacy/viewsets.py
+++ apps/person/api/legacy/viewsets.py
@@ -1,13 +1,9 @@
-from django.http import HttpResponse
-from rest_framework import generics, viewsets, permissions
-from rest_framework import filters
+from rest_framework import generics
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.permissions import DjangoObjectPermissions
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from guardian.shortcuts import assign_perm
-from datetime import datetime
-
 from . import serializers
 from apps.person.models import *
 
@@ -98,15 +94,21 @@ class PersonAddFaceView(CreateModelMixin, generics.GenericAPIView):
 
 class PersonAddTattooView(CreateModelMixin, generics.GenericAPIView):
     queryset = Tattoo.objects.all()
-    serializer_class = TattooSerializer
+    serializer_class = serializers.TattooLegacySerializer
     permission_classes = [DjangoObjectPermissions]
 
     def perform_create(self, serializer):
         person = get_object_or_404(Person, uuid=self.kwargs['uuid'])
+        user = get_object_or_404(User, username=self.kwargs['username'])
+        created_at = str(self.request.data['created_at'])
+        updated_at = str(self.request.data['updated_at'])
         if serializer.is_valid():
-            instance = serializer.save(person=person, created_by=self.request.user)
-            assign_perm("change_tattoo", self.request.user, instance)
-            assign_perm("delete_tattoo", self.request.user, instance)
+            instance = serializer.save(person=person, created_by=user)
+            instance.created_at = created_at
+            instance.updated_at = updated_at
+            instance.save()
+            assign_perm("change_tattoo", user, instance)
+            assign_perm("delete_tattoo", user, instance)
             person.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
@@ -117,15 +119,21 @@ class PersonAddTattooView(CreateModelMixin, generics.GenericAPIView):
 
 class PersonAddNicknameView(CreateModelMixin, generics.GenericAPIView):
     queryset = Nickname.objects.all()
-    serializer_class = NicknameSerializer
+    serializer_class = serializers.NicknameLegacySerializer
     permission_classes = [DjangoObjectPermissions]
 
     def perform_create(self, serializer):
         person = get_object_or_404(Person, uuid=self.kwargs['uuid'])
+        user = get_object_or_404(User, username=self.kwargs['username'])
+        created_at = str(self.request.data['created_at'])
+        updated_at = str(self.request.data['updated_at'])
         if serializer.is_valid():
-            instance = serializer.save(person=person, created_by=self.request.user)
-            assign_perm("change_nickname", self.request.user, instance)
-            assign_perm("delete_nickname", self.request.user, instance)
+            instance = serializer.save(person=person, created_by=user)
+            instance.created_at = created_at
+            instance.updated_at = updated_at
+            instance.save()
+            assign_perm("change_nickname", user, instance)
+            assign_perm("delete_nickname", user, instance)
             person.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
@@ -136,15 +144,21 @@ class PersonAddNicknameView(CreateModelMixin, generics.GenericAPIView):
 
 class PersonAddPhysicalView(CreateModelMixin, generics.GenericAPIView):
     queryset = Physical.objects.all()
-    serializer_class = PhysicalSerializer
+    serializer_class = serializers.PhysicalLegacySerializer
     permission_classes = [DjangoObjectPermissions]
 
     def perform_create(self, serializer):
         person = get_object_or_404(Person, uuid=self.kwargs['uuid'])
+        user = get_object_or_404(User, username=self.kwargs['username'])
+        created_at = str(self.request.data['created_at'])
+        updated_at = str(self.request.data['updated_at'])
         if serializer.is_valid():
-            instance = serializer.save(person=person, created_by=self.request.user)
-            assign_perm("change_physical", self.request.user, instance)
-            assign_perm("delete_physical", self.request.user, instance)
+            instance = serializer.save(person=person, created_by=user)
+            instance.created_at = created_at
+            instance.updated_at = updated_at
+            instance.save()
+            assign_perm("change_physical", user, instance)
+            assign_perm("delete_physical", user, instance)
             person.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
@@ -155,16 +169,22 @@ class PersonAddPhysicalView(CreateModelMixin, generics.GenericAPIView):
 
 class PersonAddDocumentView(CreateModelMixin, generics.GenericAPIView):
     queryset = Document.objects.all()
-    serializer_class = DocumentSerializer
+    serializer_class = serializers.DocumentLegacySerializer
     permission_classes = [DjangoObjectPermissions]
 
     def perform_create(self, serializer):
         person = get_object_or_404(Person, uuid=self.kwargs['uuid'])
+        user = get_object_or_404(User, username=self.kwargs['username'])
+        created_at = str(self.request.data['created_at'])
+        updated_at = str(self.request.data['updated_at'])
         if serializer.is_valid():
-            instance = serializer.save(created_by=self.request.user)
+            instance = serializer.save(created_by=user)
+            instance.created_at = created_at
+            instance.updated_at = updated_at
+            instance.save()
             person.documents.add(instance)
-            assign_perm("change_document", self.request.user, instance)
-            assign_perm("delete_document", self.request.user, instance)
+            assign_perm("change_document", user, instance)
+            assign_perm("delete_document", user, instance)
             person.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
@@ -175,16 +195,22 @@ class PersonAddDocumentView(CreateModelMixin, generics.GenericAPIView):
 
 class PersonAddAddressView(CreateModelMixin, generics.GenericAPIView):
     queryset = Address.objects.all()
-    serializer_class = AddressSerializer
+    serializer_class = serializers.AddressLegacySerializer
     permission_classes = [DjangoObjectPermissions]
 
     def perform_create(self, serializer):
         person = get_object_or_404(Person, uuid=self.kwargs['uuid'])
+        user = get_object_or_404(User, username=self.kwargs['username'])
+        created_at = str(self.request.data['created_at'])
+        updated_at = str(self.request.data['updated_at'])
         if serializer.is_valid():
-            instance = serializer.save(created_by=self.request.user)
+            instance = serializer.save(created_by=user)
+            instance.created_at = created_at
+            instance.updated_at = updated_at
+            instance.save()
             person.addresses.add(instance)
-            assign_perm("change_address", self.request.user, instance)
-            assign_perm("delete_address", self.request.user, instance)
+            assign_perm("change_address", user, instance)
+            assign_perm("delete_address", user, instance)
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
 
@@ -194,7 +220,7 @@ class PersonAddAddressView(CreateModelMixin, generics.GenericAPIView):
 
 class PersonAddImageView(CreateModelMixin, generics.GenericAPIView):
     queryset = Image.objects.all()
-    serializer_class = ImageSerializer
+    serializer_class = serializers.ImageLegacySerializer
     permission_classes = [DjangoObjectPermissions]
 
     def perform_create(self, serializer):
@@ -208,8 +234,8 @@ class PersonAddImageView(CreateModelMixin, generics.GenericAPIView):
             instance.updated_at = updated_at
             instance.save()
             person.images.add(instance)
-            assign_perm("change_image", self.request.user, instance)
-            assign_perm("delete_image", self.request.user, instance)
+            assign_perm("change_image", user, instance)
+            assign_perm("delete_image", user, instance)
             person.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
