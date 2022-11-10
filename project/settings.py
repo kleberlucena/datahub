81127a8c715f32a30c8a1de6568dc5fd2fdbbf43
rafaelmaia8384 <rafaelpvm@hotmail.com>
2022-11-10 14:00:50 -0300

    Reposicionado o middleware do cors para a primeira posição no array

--- project/settings.py
+++ project/settings.py
@@ -83,9 +83,9 @@ INSTALLED_APPS = [
 
 ]
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django_graylog.GraylogMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
