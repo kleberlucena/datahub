ddeed3d23483664af59ea3be8f0e575bcddf86ad
Jovennan Ramalho <jovennan@gmail.com>
2023-01-05 22:07:29 +0000

    fix error while create person

--- apps/person/helpers.py
+++ apps/person/helpers.py
@@ -9,13 +9,13 @@ logger = logging.getLogger(__name__)
 
 
 def process_external_consult(person, username, cpf=None):
-    # TODO: validar atributos do usu[ario
+    # TODO: validar atributos do usuário
     if cpf:
         try:
             retorno = tasks.cortex_consult(username=username, cpf=cpf)
             models.Registry.objects.update_or_create(system_label="CORTEX", system_uuid=retorno.uuid, person=person)
         except Exception as e:
-            raise logger.error('Error while getting registry in cortex - {}'.format(e))
+            logger.error('Error while getting registry in cortex - {}'.format(e))
     else:
         logger.info('CPF not informed or invalid - {}'.format(cpf))
 
@@ -27,5 +27,5 @@ def validate_document(number):
     except Document.DoesNotExist:
         return None
     except Exception as e:
-        raise logger.error('Error while getting document in bacinf - {}'.format(e))
+        logger.error('Error while getting document in bacinf - {}'.format(e))
         return None
