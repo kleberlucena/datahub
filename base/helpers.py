955f3aa52c7e84e8d74670c376746ab6df64c0bd
rafaelmaia8384 <rafaelpvm@hotmail.com>
2023-01-13 14:48:37 -0300

    Criado o método get_verified_check para ser usado pelo get_watermark_url na geração da URL assinada do imgproxy

--- base/helpers.py
+++ base/helpers.py
@@ -1,30 +1,38 @@
+import re
 import requests
 import json
+import urllib.parse
+import base64
+from hashlib import sha1
+from hmac import new as hmac
+from datetime import datetime, timedelta
 from django.conf import settings
 from numpy import base_repr
 
 
+def get_verified_check(sum):
+    return hmac(bytes(settings.WATERMARK_SECRET, "utf-8"), sum.encode("utf-8"), sha1).hexdigest() 
+
+
 def get_watermark_url(old_url, user_number):
 
-    if settings.DEBUG:
+    if settings.DEBUG == True:
         return old_url
     else:
-        num = base_repr(int(user_number), 36)
-        body = { 
-            "waterMarkId": num,
-            "imagePath": old_url,
-        }
-        headers = {
-            "Authorization": '9F4FE54F2555AD0AAA09E213977DAEE31A5DBAEBBB498822ECDCE5FBFF11F28B',
-            "Content-Type": "application/json",
-        }
-        response = requests.post("http://10.0.1.57:9005/get-signed-url",
-            json = body,
-            headers = headers,
-        )
-        return json.loads(response.text)['signedUrl']
-        
-
+        """"
+        A variável num pega o CPF do usuário e converte para base36.
+        Essa conversão está sendo feita enquanto não definimos qual será a string
+        que irá identificar a marca dágua do usuário.
+        """
+        num = re.sub(r"[^0-9]", "", user_number)
+        key = base_repr(int(num), 36)
+        exp = int((datetime.now() + timedelta(minutes = 10)).timestamp())
+        encoded_img_path = urllib.parse.quote_plus(old_url)
+        img_path_64 = base64.b64encode(bytes(encoded_img_path, 'utf-8')).decode("utf-8")
+        check = get_verified_check("{}{}{}".format(key, exp, img_path_64))
+        signed_url = "{}/image?check={}&key={}&exp={}&imagePath={}".format(settings.WATERMARK_HOST, check, key, exp, img_path_64)
+        print(signed_url)
+        return signed_url
 
 
 def get_image_variation(self, object, variation):
