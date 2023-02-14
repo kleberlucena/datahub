e466c9c16c871e48ed87eac1e823102a7e2f7514
rafaelmaia8384 <rafaelpvm@hotmail.com>
2023-02-14 13:43:56 -0300

    Ajustes no settings.py

--- project/settings.py
+++ project/settings.py
@@ -21,8 +21,6 @@ ALLOWED_HOSTS = list(
 CORS_URLS_REGEX = r'^/api/v1/.*$'
 if DEBUG:
     CORS_ALLOW_ALL_ORIGINS = True
-    #Project PORT
-    PROJECT_PORT = env("PROJECT_PORT")
 else:
     CORS_ALLOWED_ORIGINS = [
         "https://portal.stage.pm.pb.gov.br",
@@ -314,6 +312,7 @@ if DEBUG:
     MINIO_CONSISTENCY_CHECK_ON_START = False
     MINIO_EXTERNAL_ENDPOINT_USE_HTTPS = False
     MINIO_USE_HTTPS = False
+    PROJECT_PORT = env("PROJECT_PORT")
     # INSTALLED_APPS.append('debug_toolbar')  # module to debug
     # MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
     REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'].append('rest_framework.authentication.SessionAuthentication')
