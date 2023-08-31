3af9b95328f51dca6930a89d54ad34addc34056a
Jovennan Ramalho <jovennan@gmail.com>
2023-08-31 10:52:36 -0300

    create fact views

--- apps/fact/views.py
+++ apps/fact/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView
+from .models import *
+from base.mixins import *
+from base.views import *
+
 
 # Create your views here.
+
+class DashboardView(GroupRequiredMixin, TemplateView):
+    template_name = 'fact/dashboard.html'
+    group_required = ['fact:fact_basic',
+                      'fact:fact_intermediate', 'fact:fact_advanced']
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        user = request.user
+        my_facts = Fact.objects.filter(created_by=user)[:3]
+        facts = Fact.objects.all()
+
+        context = {"my_facts": my_facts, "facts": facts,}
+        return self.render_to_response(context)
--- base/views.py
+++ base/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, Group
+
+
 
 import logging
 logger = logging.getLogger(__name__)
@@ -17,3 +21,23 @@ class IndexView(TemplateView):
 
 class AboutView(TemplateView):
     template_name = 'base/about.html'
+
+class IndexView(TemplateView):
+    def get(self, request):
+        user = request.user
+        group_list = Group.objects.filter(user=user).values()
+        if any("fact" in item['name'] for item in group_list):
+            return redirect('fact:index')
+
+        if any("profile:person" in item['name'] for item in group_list):
+            return redirect('person:list_person')
+
+        return render(request, 'base/index.html',)
+
+
+class AboutView(TemplateView):
+    template_name = 'base/about.html'
+
+
+def authorization_error_view(request):
+    return render(request, 'base/error_template.html', {'message': 'Você não tem permissão para acessar essa página'})
