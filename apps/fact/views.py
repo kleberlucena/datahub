2506952470f6b9d1102be083a1cf45c7d3af7b21
Jovennan Ramalho <jovennan@gmail.com>
2023-09-13 16:52:10 -0300

    create forms fact-type

--- apps/fact/views.py
+++ apps/fact/views.py
@@ -1,11 +1,84 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
 from .models import *
+from .forms import *
 from base.mixins import *
 from base.views import *
 
 
-# Create your views here.
+from .models import Fact
+
+
+
+class FactImageListView(ListView):
+    model = FactImage
+    template_name = 'fact_image_list.html'
+    context_object_name = 'fact_images'
+
+class FactImageCreateView(CreateView):
+    model = Fact
+    form_class = FactForm
+    template_name = 'fact_image_form.html'
+    success_url = reverse_lazy('fact-image-list')
+
+class FactImageUpdateView(UpdateView):
+    model = FactImage
+    form_class = FactForm
+    template_name = 'fact_image_form.html'
+    success_url = reverse_lazy('fact-image-list')
+
+class FactImageDeleteView(DeleteView):
+    model = FactImage
+    template_name = 'fact_image_confirm_delete.html'
+    success_url = reverse_lazy('fact-image-list')
+
+
+class FactTypeListView(ListView):
+    model = FactType
+    template_name = 'fact_type_list.html'
+    context_object_name = 'fact_types'
+
+class FactTypeCreateView(CreateView):
+    model = FactType
+    form_class = FactForm
+    template_name = 'fact_type_form.html'
+    success_url = reverse_lazy('fact-type-list')
+
+class FactTypeUpdateView(UpdateView):
+    model = FactType
+    form_class = FactForm
+    template_name = 'fact_type_form.html'
+    success_url = reverse_lazy('fact-type-list')
+
+class FactTypeDeleteView(DeleteView):
+    model = FactType
+    template_name = 'fact_type_confirm_delete.html'
+    success_url = reverse_lazy('fact-type-list')
+
+
+class FactListView(ListView):
+    model = Fact
+    template_name = 'fact_list.html'
+    context_object_name = 'facts'
+
+class FactCreateView(CreateView):
+    model = Fact
+    form_class = FactForm
+    template_name = 'fact_form.html'
+    success_url = reverse_lazy('fact-list')
+
+class FactUpdateView(UpdateView):
+    model = Fact
+    form_class = FactForm
+    template_name = 'fact_form.html'
+    success_url = reverse_lazy('fact-list')
+
+class FactDeleteView(DeleteView):
+    model = Fact
+    template_name = 'fact_confirm_delete.html'
+    success_url = reverse_lazy('fact-list')
+
 
 class DashboardView(GroupRequiredMixin, TemplateView):
     template_name = 'fact/dashboard.html'
--- project/settings.py
+++ project/settings.py
@@ -104,6 +104,7 @@ INSTALLED_APPS = [
     'django_celery_beat',
     'celery_progress',
     'guardian',
+    'leaflet',
     'corsheaders',
     'easyaudit',
 
