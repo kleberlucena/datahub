bc933cb1de53d8b38afba1a0ba14c9c6f7f5f783
Marcos Paiva <marcospaivajr7@gmail.com>
2023-11-18 17:13:11 -0300

    feat: task de sincronizar military e user adicionada

--- apps/portal/tasks.py
+++ apps/portal/tasks.py
@@ -8,16 +8,17 @@ from celery import shared_task
 from celery_progress.backend import ProgressRecorder
 import logging
 
-from .models import Entity, Military, HistoryTransfer, Promotion
-from .helpers import getMilitaryImageFile
-from .services import get_data_entity_api_portal, get_data_military_api_portal
+# from .models import Entity, Military, HistoryTransfer, Promotion
+# from .helpers import getMilitaryImageFile
+# from .services import get_data_entity_api_portal, get_data_military_api_portal
+from . import models, helpers, services
 
 logger = logging.getLogger(__name__)
 
 
 @shared_task(bind=True)
 def task_get_entity_from_portal(self):
-    data = get_data_entity_api_portal(0)
+    data = services.get_data_entity_api_portal(0)
     progress_recorder = ProgressRecorder(self)
     if data:
         # Gets an extra page to receive data left over from division by 10
@@ -27,7 +28,7 @@ def task_get_entity_from_portal(self):
                 results = data['results']
                 for result in results:
                     try:
-                        entity = Entity.objects.get(id_portal=result['id'])
+                        entity = models.Entity.objects.get(id_portal=result['id'])
                         entity.name = result['name']
                         entity.code = result['code']
                         entity.father = result['father']
@@ -35,20 +36,20 @@ def task_get_entity_from_portal(self):
                         entity.category = result['category']
                         entity.hierarchy = result['hierarchy']
                         entity.save()
-                    except Entity.DoesNotExist:
+                    except models.Entity.DoesNotExist:
                         logger.info('Unidade nova vinda do portal - {}'.format(result['id']))
-                        Entity.objects.create(id_portal=result['id'], name=result['name'], code=result['code'], father=result['father'],
+                        models.Entity.objects.create(id_portal=result['id'], name=result['name'], code=result['code'], father=result['father'],
                                                         child_exists=result['child_exists'], category=result['category'],
                                                         hierarchy=result['hierarchy'])
                     except IntegrityError as ie:
                         logger.error('Unidade com erro de integridade - {}'.format(ie))
-                        Entity.objects.filter(id_portal=result['id']).update(name=result['name'], code=result['code'], father=result['father'],
+                        models.Entity.objects.filter(id_portal=result['id']).update(name=result['name'], code=result['code'], father=result['father'],
                                                                                      child_exists=result[
                                                                                          'child_exists'], category=result['category'],
                                                                                      hierarchy=result['hierarchy'])
                     except Exception as e:
                         logger.error("Error, {}".format(e))
-                data = get_data_entity_api_portal(i * 10)
+                data = services.get_data_entity_api_portal(i * 10)
                 total_percents = ((i / 10) * 100)
                 progress_recorder.set_progress(
                     i + 1, 10, f'Concluído {total_percents}%')
@@ -58,7 +59,7 @@ def task_get_entity_from_portal(self):
 
 @shared_task(bind=True)
 def task_get_military_from_portal(self):
-    datas = get_data_military_api_portal(0)
+    datas = services.get_data_military_api_portal(0)
     progress_recorder = ProgressRecorder(self)
     if datas:
         quantidade_paginas = int(datas['count'] / 10) + 2
@@ -71,7 +72,7 @@ def task_get_military_from_portal(self):
                     unit = None
                     url_image = None
                     try:
-                        unit = Entity.objects.get(code=result['unit'])
+                        unit = models.Entity.objects.get(code=result['unit'])
                     except Exception as e:
                         unit = None
                     if result['image']:
@@ -79,13 +80,10 @@ def task_get_military_from_portal(self):
                     else:
                         url_image = 'https://imgur.com/jS8iL9p'
                         
-                    militaryInBacinf = Military.objects.filter(
+                    militaryInBacinf = models.Military.objects.filter(
                         register=result['register'])
                     if militaryInBacinf.exists():
-                        print('Unidade anterir')
-                        print(militaryInBacinf)
-                        print('__________________')
-                        military = Military.objects.filter(
+                        military = models.Military.objects.filter(
                             register=result['register']).first()
 
                         if military:
@@ -120,48 +118,47 @@ def task_get_military_from_portal(self):
                                 military.user = user
 
                             # Saving image from military
-                            image_file = getMilitaryImageFile(result['image'])
+                            image_file = helpers.getMilitaryImageFile(result['image'])
                             if image_file:
                                 military.image.save("{}.jpg".format(
                                     military.cpf), files.File(open(image_file[0], 'rb')))
 
-                        MilitaryHistoryTransfer = HistoryTransfer.objects.filter(
+                        MilitaryHistoryTransfer = models.HistoryTransfer.objects.filter(
                             military__register=result['register'])
-                        print('Unidade nova')
-                        print(military)
+
 
                         if not (MilitaryHistoryTransfer.filter(
                                 entity__code=result['unit'])):
 
-                            HistoryTransfer.objects.filter(
+                            models.HistoryTransfer.objects.filter(
                                 military__register=result['register'], date_finish__isnull=True).update(
                                 date_finish=date.now(),
                                 obs="Militar saiu da unidade")
                             try:
-                                unit = Entity.objects.get(code=result['unit'])
-                                mili = Military.objects.get(
+                                unit = models.Entity.objects.get(code=result['unit'])
+                                mili = models.Military.objects.get(
                                     register=result['register'])
-                                HistoryTransfer.objects.create(
+                                models.HistoryTransfer.objects.create(
                                     military=mili, entity=unit, date_start=date.now(), obs="Militar mudou de unidade")
-                            except Entity.DoesNotExist:
+                            except models.Entity.DoesNotExist:
                                 logger.error('Unidade não encotrada - {}'.format(result['unit']))
-                            except Military.DoesNotExist:
+                            except models.Military.DoesNotExist:
                                 logger.error('Militar não encontrado - {}'.format(result['register']))
 
-                        MilitaryPromotion = Promotion.objects.filter(
+                        MilitaryPromotion = models.Promotion.objects.filter(
                             military__register=result['register'])
 
                         if not (MilitaryPromotion.filter(
                                 rank=result['rank'])):
-                            mili = Military.objects.get(
+                            mili = models.Military.objects.get(
                                 register=result['register'])
-                            Promotion.objects.create(
+                            models.Promotion.objects.create(
                                 military=mili, rank=result['rank'])
 
                     else:
 
                         # unit = Entity.objects.get(code=result['unit'])
-                        military = Military.objects.create(
+                        military = models.Military.objects.create(
                             entity=unit,
                             name=result['name'],
                             admission_date=result['admission_date'],
@@ -194,22 +191,38 @@ def task_get_military_from_portal(self):
                         # Saving image from military
                         try:
                             if url_image is not None:
-                                image_file = getMilitaryImageFile(url_image)
+                                image_file = helpers.getMilitaryImageFile(url_image)
                                 military.image.save("{}.jpg".format(
                                 military.cpf), files.File(open(image_file[0], 'rb')))
                         except Exception as e:
                             logger.error('Erro de inesperado ao salvar imagem - {}'.format(e))
 
-                        HistoryTransfer.objects.create(
+                        models.HistoryTransfer.objects.create(
                             military=military, entity=unit, date_start=date.now(), obs="Primeiro cadastro")
 
-                        Promotion.objects.create(
+                        models.Promotion.objects.create(
                             military=military, rank=result['rank'])
 
-                datas = get_data_military_api_portal(i * 10)
+                datas = services.get_data_military_api_portal(i * 10)
                 total_percents = ((i / 10) * 100)
                 progress_recorder.set_progress(
                     i + 1, 10, f'Concluído {total_percents}%')
 
     else:
         progress_recorder.set_progress(0, 0, f'Concluído 100%')
+
+
+@shared_task(bind=True)
+def link_military(self, username):
+    """
+    Relaciona o militar a um usuário através de seu username
+    """
+    
+    militaries = models.Military.objects.filter(cpf=username, user__isnull=True)
+    
+    for military in militaries:
+        user = User.objects.get(username=username)
+        military.user = user
+        military.save()
+        logger.info('Military linked successful with User Django behind username: {}'.format(username))
+ 
