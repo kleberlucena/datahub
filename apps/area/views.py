695cfac2ab8dd2d0e57ad16a122162d4ab308b39
Nasser <nfilho@live.com>
2023-12-22 17:36:49 -0300

    views urls qpp cpr

--- apps/area/views.py
+++ apps/area/views.py
@@ -50,3 +50,73 @@ class DeleteAreaView(DeleteView):
         context = super().get_context_data(**kwargs)
         context['object_to_delete'] = self.get_object()
         return context
+    
+    
+#     # View de Listagem para Qpp
+# class QppListView(ListView):
+#     model = models.Qpp
+#     template_name = 'qpp/qpps.html'
+
+#     def get_context_data(self, *args, **kwargs):
+#         context = super(QppListView, self).get_context_data(**kwargs)
+#         qpps = models.Qpp.objects.all()
+#         context['qpps'] = qpps
+#         return context
+
+# # View de Criação para Qpp
+# class CreateQppView(CreateView):
+#     model = models.Qpp
+#     form_class = forms.QppForm
+#     template_name = 'qpp/qpp_form.html'
+#     success_url = reverse_lazy('qpp:qpp_list')
+
+# # View de Atualização para Qpp
+# class UpdateQppView(UpdateView):
+#     model = models.Qpp
+#     template_name = 'qpp/qpp_form.html'
+#     form_class = forms.QppForm
+#     success_url = reverse_lazy('qpp:qpp_list')
+
+# # View de Exclusão para Qpp
+# class DeleteQppView(DeleteView):
+#     model = models.Qpp
+#     template_name = 'qpp/qpp_delete.html'
+#     success_url = reverse_lazy('qpp:qpp_list')
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['object_to_delete'] = self.get_object()
+#         return context
+
+# # Views para Cpr
+# class CprListView(ListView):
+#     model = models.Cpr
+#     template_name = 'cpr/cprs.html'
+
+#     def get_context_data(self, *args, **kwargs):
+#         context = super(CprListView, self).get_context_data(**kwargs)
+#         cprs = models.Cpr.objects.all()
+#         context['cprs'] = cprs
+#         return context
+
+# class CreateCprView(CreateView):
+#     model = models.Cpr
+#     form_class = forms.CprForm
+#     template_name = 'cpr/cpr_form.html'
+#     success_url = reverse_lazy('cpr:cpr_list')
+
+# class UpdateCprView(UpdateView):
+#     model = models.Cpr
+#     template_name = 'cpr/cpr_form.html'
+#     form_class = forms.CprForm
+#     success_url = reverse_lazy('cpr:cpr_list')
+
+# class DeleteCprView(DeleteView):
+#     model = models.Cpr
+#     template_name = 'cpr/cpr_delete.html'
+#     success_url = reverse_lazy('cpr:cpr_list')
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['object_to_delete'] = self.get_object()
+#         return context
