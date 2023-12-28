93d878e961bfc7264bf52fd5fd909f409fceec59
filho <nfilho@live.com>
2023-12-28 17:02:47 -0300

    model area update

--- apps/area/forms.py
+++ apps/area/forms.py
@@ -5,12 +5,12 @@ from . import models
 class AreaForm(forms.ModelForm):
     class Meta:
         model = models.Area
-        fields = ['name','details']
+        fields = ['name','description']
         labels = {
             'name': 'Nome da area',
-            'details': 'Informações adicionais',
+            'description': 'Informações adicionais',
         }
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ex. 1BPM ou QPP1 ou CPRM'}), #ESTÁ DE MANEIRA GENÉRICA
-            'details': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Se houver, insira informações adicionais'}),
+            'description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Se houver, insira informações adicionais'}),
         }
