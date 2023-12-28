4398e7f865ec7481574da2dbedb740b5223e2bd6
filho <nfilho@live.com>
2023-12-28 14:56:30 -0300

    urls

--- apps/area/models.py
+++ apps/area/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class Area(models.Model):
-    name = models.CharField("Nome", max_length=50)
-    details = models.CharField("Descrição", max_length=300, null=True, blank=True)
+    name = models.CharField("Nome", max_length=50) #1BPM
+    details = models.CharField("Descrição", max_length=300, null=True, blank=True) #Área do 1 Batalhão
+    #cpr #CPRM
+    #qpps = #QPP1,QPP2,QPP3,QPP4
 
     def __str__(self):
         return self.name
--- apps/area/urls.py
+++ apps/area/urls.py
@@ -12,14 +12,14 @@ urlpatterns = [
   path('<int:pk>/area/update', views.UpdateAreaView.as_view(), name='area_update'),
   path('<int:pk>/area/delete', views.DeleteAreaView.as_view(), name='area_delete'),
 
-#   path('qpp/list/', views.QppListView.as_view(), name='qpp_list'),  
-#   path('qpp/add/', views.CreateQppView.as_view(), name='qpp_add'), 
-#   path('<int:pk>/qpp/update', views.UpdateQppView.as_view(), name='qpp_update'),
-#   path('<int:pk>/qpp/delete', views.DeleteQppView.as_view(), name='qpp_delete'),
+  path('qpp/list/', views.QppListView.as_view(), name='qpp_list'),  
+  path('qpp/add/', views.CreateQppView.as_view(), name='qpp_add'), 
+  path('<int:pk>/qpp/update', views.UpdateQppView.as_view(), name='qpp_update'),
+  path('<int:pk>/qpp/delete', views.DeleteQppView.as_view(), name='qpp_delete'),
 
-#   path('cpr/list/', views.CprListView.as_view(), name='cpr_list'),  
-#   path('cpr/add/', views.CreateCprView.as_view(), name='cpr_add'), 
-#   path('<int:pk>/cpr/update', views.UpdateCprView.as_view(), name='cpr_update'),
-#   path('<int:pk>/cpr/delete', views.DeleteCprView.as_view(), name='cpr_delete'),
+  path('cpr/list/', views.CprListView.as_view(), name='cpr_list'),  
+  path('cpr/add/', views.CreateCprView.as_view(), name='cpr_add'), 
+  path('<int:pk>/cpr/update', views.UpdateCprView.as_view(), name='cpr_update'),
+  path('<int:pk>/cpr/delete', views.DeleteCprView.as_view(), name='cpr_delete'),
 
-# ]
+]
