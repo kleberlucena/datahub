9a73119779c5003b7158682c0a7dd717f85bf8ab
filho <nfilho@live.com>
2024-01-22 23:46:58 -0300

    add spot to model

--- apps/georeference/admin.py
+++ apps/georeference/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.contrib.gis import admin as geo_admin
-
 from apps.georeference.models import *
+from apps.georeference import models
 
 
 @admin.register(Area)
 class AreaAdmin(geo_admin.OSMGeoAdmin):
     list_display = ('name', "description", "area_polygon")
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', "description")
+admin.site.register(models.Category)
+admin.site.register(models.SpotType)
+admin.site.register(models.Spot)
+admin.site.register(models.SpotAddresses)
