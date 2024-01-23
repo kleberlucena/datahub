17d23c6d69573ac13b1ec5df6d383a1160beca67
filho <nfilho@live.com>
2024-01-23 12:25:03 -0300

    permission spot update

--- apps/georeference/management/commands/georeference_seed.py
+++ apps/georeference/management/commands/georeference_seed.py
@@ -12,17 +12,20 @@ class Command(BaseCommand):
 
     def create_groups(self):
         groups_to_create = [
-            "profile:georeference_basic", #user can list areas and categories
-            "profile:georeference_advanced", #user can list/create/update areas and categories
-            "profile:georeference_manager", #user can list/create//delete areas and categories
+            "profile:georeference_area_basic", #user can list areas and categories
+            "profile:georeference_area_advanced", #user can list/create/update areas and categories
+            "profile:georeference_area_manager", #user can list/create/delete areas and categories
+            "profile:georeference_spot_basic", #user can list spottypes, spots and spotaddresses
+            "profile:georeference_spot_advanced", #user can list/create/update spottypes, spots and spotaddresses
+            "profile:georeference_spot_manager", #user can list/create/delete spottypes, spots and spotaddresses
         ]
 
-        permissions_to_assign_basic = [
+        permissions_to_assign_area_basic = [
             "view_area",
             "view_category"
         ]
 
-        permissions_to_assign_advanced = [
+        permissions_to_assign_area_advanced = [
             "view_area",
             "add_area",
             "change_area",
@@ -31,7 +34,7 @@ class Command(BaseCommand):
             "change_category",
         ]
 
-        permissions_to_assign_manager = [
+        permissions_to_assign_area_manager = [
             "view_area",
             "add_area",
             "change_area",
@@ -40,6 +43,39 @@ class Command(BaseCommand):
             "add_category",
             "change_category",
             "delete_category",
+        ]
+
+        permissions_to_assign_spot_basic = [
+            "view_spottype",
+            "view_spotaddresses",
+            "view_spot",
+        ]
+
+        permissions_to_assign_spot_advanced = [
+            "view_spottype",
+            "view_spotaddresses",
+            "view_spot",
+            "add_spottype",
+            "add_spotaddresses",
+            "add_spot",
+            "change_spottype",
+            "change_spotaddresses",
+            "change_spot",
+        ]
+
+        permissions_to_assign_spot_manager = [
+            "view_spottype",
+            "view_spotaddresses",
+            "view_spot",
+            "add_spottype",
+            "add_spotaddresses",
+            "add_spot",
+            "change_spottype",
+            "change_spotaddresses",
+            "change_spot"
+            "delete_spottype",
+            "delete_spotaddresses",
+            "delete_spot",
         ]
 
 
@@ -53,35 +89,68 @@ class Command(BaseCommand):
                 else:
                     self.stdout.write(self.style.WARNING(f'Group "{group_name}" already exists.'))
 
-                # Assign permissions to "profile:georeference_basic" group
-                if group_name == "profile:georeference_basic":
+                # Assign permissions to "profile:georeference_area_basic" group
+                if group_name == "profile:georeference_area_basic":
                     permissions = []
                     for model in models_to_get:
                         content_type = ContentType.objects.get_for_model(model)
                         model_permissions = Permission.objects.filter(
-                            content_type=content_type, codename__in=permissions_to_assign_basic
+                            content_type=content_type, codename__in=permissions_to_assign_area_basic
                         )
                         permissions.extend(model_permissions)
                     group.permissions.add(*permissions)
 
-                # Assign permissions to "profile:georeference_advanced" group
-                if group_name == "profile:georeference_advanced":
+                # Assign permissions to "profile:georeference_area_advanced" group
+                if group_name == "profile:georeference_area_advanced":
                     permissions = []
                     for model in models_to_get:
                         content_type = ContentType.objects.get_for_model(model)
                         model_permissions = Permission.objects.filter(
-                            content_type=content_type, codename__in=permissions_to_assign_advanced
+                            content_type=content_type, codename__in=permissions_to_assign_area_advanced
                         )
                         permissions.extend(model_permissions)
                     group.permissions.add(*permissions)
 
-                # Assign permissions to "profile:georeference_manager" group
-                if group_name == "profile:georeference_manager":
+                # Assign permissions to "profile:georeference_area_manager" group
+                if group_name == "profile:georeference_area_manager":
                     permissions = []
                     for model in models_to_get:
                         content_type = ContentType.objects.get_for_model(model)
                         model_permissions = Permission.objects.filter(
-                            content_type=content_type, codename__in=permissions_to_assign_manager
+                            content_type=content_type, codename__in=permissions_to_assign_area_manager
+                        )
+                        permissions.extend(model_permissions)
+                    group.permissions.add(*permissions)
+
+                # Assign permissions to "profile:georeference_spot_basic" group
+                if group_name == "profile:georeference_spot_basic":
+                    permissions = []
+                    for model in models_to_get:
+                        content_type = ContentType.objects.get_for_model(model)
+                        model_permissions = Permission.objects.filter(
+                            content_type=content_type, codename__in=permissions_to_assign_spot_basic
+                        )
+                        permissions.extend(model_permissions)
+                    group.permissions.add(*permissions)
+
+                # Assign permissions to "profile:georeference_spot_advanced" group
+                if group_name == "profile:georeference_spot_advanced":
+                    permissions = []
+                    for model in models_to_get:
+                        content_type = ContentType.objects.get_for_model(model)
+                        model_permissions = Permission.objects.filter(
+                            content_type=content_type, codename__in=permissions_to_assign_spot_advanced
+                        )
+                        permissions.extend(model_permissions)
+                    group.permissions.add(*permissions)
+
+                # Assign permissions to "profile:georeference_spot_manager" group
+                if group_name == "profile:georeference_spot_manager":
+                    permissions = []
+                    for model in models_to_get:
+                        content_type = ContentType.objects.get_for_model(model)
+                        model_permissions = Permission.objects.filter(
+                            content_type=content_type, codename__in=permissions_to_assign_area_manager
                         )
                         permissions.extend(model_permissions)
                     group.permissions.add(*permissions)
